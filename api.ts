e6e16d13d78444f0e4824cbe717d5b93ebde6caa
Richard Knoll <riknoll@users.noreply.github.com>
2020-04-29 09:46:56 -0700

    Initial files for MakeCode project

---
+++ api.ts
@@ -0,0 +1,27 @@
+//% block="story" color="#3b9934"
+namespace story {
+    //% block="create scene"
+    export function createScene(cb: () => void) {
+
+    }
+
+    //% block="with part" handlerParameter
+    export function addScenePart(cb: () => void) {
+
+    }
+
+    //% block="$sprite say dialog $dialog"
+    export function spriteSayDialog(sprite: Sprite, dialog: string[]) {
+
+    }
+
+    //% block="$sprite walk to location $location"
+    export function spriteWalkTo(sprite: Sprite, location: tiles.Location) {
+
+    }
+
+    //% block="show text $dialog at x $x y $y"
+    export function showDialog(dialog: string[], x: number, y: number) {
+
+    }
+}
