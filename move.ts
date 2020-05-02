33c78d4c172f2e25a749d3b9bdb314b652f96487
Richard Knoll <riknoll@users.noreply.github.com>
2020-05-01 19:29:00 -0700

    Update pxt.json, move.ts

---
+++ move.ts
@@ -0,0 +1,9 @@
+namespace story {
+    export function spriteMoveToTile(sprite: Sprite, location: tiles.Location) {
+
+    }
+
+    export function spriteMoveToLocation(sprite: Sprite, x: number, y: number) {
+
+    }
+}
